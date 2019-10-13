4487db4fd7e18eae3764a9a286554b522f280c5b
Phudal <080_075@naver.com>
2019-10-13 18:28:58 +0900

    10/13

---
+++ 홍요섭_unity/New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 플레이어가 이동할 때 적용될 속도
+    public float m_MoveSpeed = 10.0f;
+    // 변수(필드)를 public 으로 선언할 경우, 인스펙터창에 노출
+
+    // 방향 벡터를 저장할 변수
+    private Vector2 _DirectionVector = Vector2.zero;
+
+    // 유니티에서 오브젝트 위치를 변경하는 3가지 방법
+    // 1) 오브젝트의 좌표를 특정 좌표로 변경
+    // position = Vector
+    // 2) 특정한 방향으로 이동
+    // position += Vector / Translate()
+    // 3) 물리엔진 이용해서 특정한 방향으로 힘을 주어 이동
+    // Regidbody2D.velocity / AddForce()
+
+
+    // Start, Update등의 함수는 위에 있어야함
+
+
+
+    // 특정 좌표로 움직임
+    private void Start()
+    {
+        // SetPosition();
+
+
+    }
+    private void Update()
+    {
+
+        MovePosition();
+    }
+    private void SetPosition()
+    {
+        gameObject.transform.position = new Vector2(10.0f, 10.0f);
+    }
+        // 특정한 방향으로 이동
+    private void MovePosition()
+    {
+        // 월드 기준(_MoveSpeed, 0.0f) 만큼 이동
+        //transform.position = new Vector2(m_MoveSpeed, 0.0f);
+        transform.Translate(new Vector2(m_MoveSpeed, 0.0f) * Time.deltaTime, Space.World);  // Space.World가 relateiveTo
+        // transfrom.Translate(translate, relativeTo);
+        // relative기준으로 translate 이동을 시킵니다.
+        // translation : 이동시킬 거리
+        // relativeTo : Space.self : 기준을 자기 자신으로 설정
+        //              Space.World : 기준을 월드로 설정
+
+        // Time.deltaTime?
+        // 이전 프레임에서 다음 프레임까지 걸린 시간 간격에 대한 읽기 전용 프로퍼티
+        // 다른 PC와의 사양이 다르더라도 실행 결과가 다르지 않도록 하기 위해서
+        //
+        // 1번 PC : 1초 60 프레임 -> Time.deltaTime = 0.0166 -> 16.66
+        // 2번 PC : 1초 30 프레임 -> Time.deltaTime = 0.0333 -> 33.33
+        //
+        // 1초 뒤에 1번 PC의 캐릭터가 이동한 거리 10(속도) * 60 = 600
+        // 1초 뒤에 2번 PC의 캐릭터가 이동한 거리 10(속도) * 30 = 300
+        //
+        // 1초 뒤에 1번 PC의 캐릭터가 이동한 거리 10(속도) * 60 = 600 * 16.66 = 9996
+        // 1초 뒤에 2번 PC의 캐릭터가 이동한 거리 10(속도) * 30 = 300 * 33.33 = 9999
+
+    }
+}
---
+++ 홍요섭_unity/New Unity Project/Assets/Scripts/UnityStartUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityStartUp : MonoBehaviour
+{
+    // 맨 처음 실행시켰을 때 한번 호출이 됨
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    // 오브젝트가 활성화 되었을 경우 한번 호출
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+    // OnEnable이 실행된 다음에 한번 호출
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+    // 호출이 규칙적입니다
+    private void FixedUpdate()
+    {
+        Debug.Log("Fixedupdate");
+    }
+    // 호출이 불규칙적
+    private void Update()
+    {
+        Debug.Log("Update");
+    }
+    // FixedUpdate, Update가 호출된 뒤 호출
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate");
+    }
+    // 오브젝트가 비활성화 되었을때 호출
+    private void OnDisable()
+    {
+        
+    }
+    // 오브젝트가 삭제되었을때 호출
+    private void OnDestroy()
+    {
+        
+    }
+}
